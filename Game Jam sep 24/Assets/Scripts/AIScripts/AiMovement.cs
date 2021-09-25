eac0650260acb70dd84e50b90077c45d72a6cefe
JackofAllTradez7 <57196083+JackofAllTradez7@users.noreply.github.com>
2021-09-25 19:22:31 -0400

    NPC patrols

---
+++ Game Jam sep 24/Assets/Scripts/AIScripts/AiMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiMovement : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float waitTime = 2f;
+    int patrolIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if(waypoints != null &&  waypoints.Count >= 2)
+        {
+            patrolIndex = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance <= 1f)
+        {
+            waitTime -= Time.deltaTime;
+            ChangeWaypoint();
+            ChangeDestination();
+        }
+       // agent.destination = waypoints.Count.position;
+    }
+
+    private void ChangeWaypoint()
+    {
+        patrolIndex++;
+
+        if(patrolIndex >= waypoints.Count)
+        {
+            patrolIndex = 0;
+        }
+    }
+
+    private void ChangeDestination()
+    {
+        if(waypoints != null)
+        {
+            Vector3 target = waypoints[patrolIndex].transform.position;
+            agent.SetDestination(target);
+            waitTime = 2f;
+        }
+    }
+
+
+}
