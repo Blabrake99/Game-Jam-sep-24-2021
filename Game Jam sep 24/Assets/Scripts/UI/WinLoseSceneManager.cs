c0fe77375ba9635ccdc90760dcd24ed739768792
Brandon <Blabrake1@gmail.com>
2021-09-26 15:10:30 -0400

    Added buttons to win lose screen and made the highlighted color for buttons darker

---
+++ Game Jam sep 24/Assets/Scripts/UI/WinLoseSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseSceneManager : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
